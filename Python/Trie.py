9e9c763aac0c6f6c6b0ab566c2ecb9587fb7db76
Théo Roncalli <theo.roncalli@yahoo.fr>
2022-01-23 23:07:00 +0100

    :rocket: Creation of the trie class

--- Python/Trie.py
+++ Python/Trie.py
@@ -2,64 +2,73 @@
 
 from ete3 import Tree
 
-def trie_insert(trie, word, index):
-    """
-    Insert word in a trie and add its index.
+class Trie(Tree):
     
-    Input:
-        trie: a trie (ete3.Tree)
-        word: a string
-        index: an integer corresponding to the word index
-    
-    Result:
-        Modifies input trie. It inserts new branches in alphabetical order
-    """
-    
-    if not word:
-        if hasattr(trie, "index"):
-            trie.index.append(index)
+    def __init__(self, name = None, words = None):
+        
+        if name:
+            super().__init__(name = name)
         else:
-            trie.add_features(index = [index])
-    else:
-        pos = -1
-        for i, child in enumerate(trie.children):
-            if child.name >= word[0]:
-                pos = i
-                break
+            super().__init__()
         
-        if pos == -1:
-            trie.children.append(Tree(name = word[0]))
-        elif trie.children[pos].name != word[0]:
-            trie.children.insert(pos, Tree(name = word[0]))
+        self.max_index = 0
         
-        trie_insert(trie.children[pos], word[1:], index)
+        if words:
+            for index, word in enumerate(words):
+                self.insert(word, index)
+            self.max_index = index
+    
+    def __str__(self):
+        return self.get_ascii(show_internal = True, attributes=["name", "index"])
+    
+    def add(self, words):
+        """
+        Construct a trie from a list of words
+        
+        Input:
+            words: a list of strings
+        
+        Output:
+            trie containing the words, annotated with the words indices
+        """
+        
+        index = self.max_index
+        for word in words:
+            index += 1
+            self.insert(word, index)
+    
+    def insert(self, word, index):
+        """
+        Insert word in a trie and add its index.
+        
+        Input:
+            trie: a trie (ete3.Tree)
+            word: a string
+            index: an integer corresponding to the word index
+        
+        Result:
+            Modifies input trie. It inserts new branches in alphabetical order
+        """
+    
+        if word:
+            pos = -1
+            for i, child in enumerate(self.children):
+                if child.name >= word[0]:
+                    pos = i
+                    break
+            
+            if pos == -1:
+                self.children.append(Trie(name = word[0]))
+            elif self.children[pos].name != word[0]:
+                self.children.insert(pos, Trie(name = word[0]))
+            
+            self.children[pos].insert(word[1:], index)
+    
+        else:
+            if hasattr(self, "index"):
+                self.index.append(index)
+            else:
+                self.add_features(index = [index])
+        
+        
 
-def trie_construct(words):
-    """
-    Construct a trie from a list of words
-    
-    Input:
-        words: a list of strings
-    
-    Output:
-        trie containing the words, annotated with the words indices
-    """
-
-    trie = Tree()
-    
-    for index, word in enumerate(words):
-        trie_insert(trie, word, index)
-
-    return trie
-
-def trie_print(trie):
-    """
-    Print trie
-    
-    Input:
-        trie: a trie (ete3.Tree) with annotation
-    
-    Result:
-        Print a trie to the stdout.
-    """
-    print(trie.get_ascii(show_internal = True, attributes=["name", "index"]))
