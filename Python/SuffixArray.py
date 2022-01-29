891eb8421c2baccabecf641d3f24eedd3ab121aa
Théo Roncalli <theo.roncalli@yahoo.fr>
2022-01-29 12:45:58 +0100

    :zap: Improve computation time for building suffix array

--- Python/SuffixArray.py
+++ Python/SuffixArray.py
@@ -1,89 +1,47 @@
 #!/usr/bin/env python3
 
-import Trie
+ALPHABET = [chr(i) for i in range(128)]
+ALPHABET.insert(0, ALPHABET.pop(ALPHABET.index("$")))
+POS = {c : p for (p, c) in enumerate(ALPHABET)}
+
+class SaStrOrder:
+    def __init__(self, inner):
+        self.inner = inner
+        
+    def __str__(self):
+        return self.inner
+
+    def __lt__(self, other):
+        for i in range(min(len(self.inner), len(other.inner))):
+            a = POS.get(self.inner[i])
+            b = POS.get(other.inner[i])
+            if a != b:
+                return a < b
+        return len(self.inner) < len(other.inner)
 
 class SuffixArray(list):
     
-    def __init__(self, text):
+    def __init__(self, text = None, trie = None):
         
         super().__init__()
-        trie = Trie.SuffixTree(text)
-        self.from_trie(trie)
+        
+        if text:
+            pairs = list()
+            for index in range(len(text)):
+                pairs.append((index, text[index:]))
+            pairs.sort(key = lambda x : x[1])
+            self.extend([i for (i, _) in pairs])
+        elif not text and trie:
+            self.from_trie(trie)
+        else:
+            raise TypeError("SuffixArray __init__() takes at lease one argument: text (a string) or trie")
         
     def from_trie(self, trie):
             
-        children = trie.children
-        
-        if children:
-            for child in children:
-                self.from_trie(child)
-        else:
-            self.extend(trie.index)
-
-    def pattern_search(self, text, pattern):
-        """
-        Find occurrences of a pattern in a text.
-        Input:
-            suffixarray: a suffix array
-            text: a string
-            pattern : a string
-        Output:
-            list of positions where the pattern occurs
-        """
-        
-        start = 0
-        end = len(self)
-        
-        def narrow_start(idx, letter):
-            for j in range(start, end):
-                if text[self[j] + idx] == letter:
-                    return j
-            return end
-    
-        def narrow_end(idx, letter):
-            for j in reversed(range(start, end)):
-                if text[self[j] + idx] == letter:
-                    return j+1
-            return start
-    
-        for idx, c in enumerate(pattern):
-            start = narrow_start(idx, c)
-            end   = narrow_end(idx, c)
-
-        if start >= end:
-            return None
-        
-        return sorted(self[start:end])
-
-    def to_bwt(self, text):
-        """
-        Construct the Burrows Wheeler transform (BWT)
-        Input:
-            text: a string
-        Output:
-            the BW-transformation (a string)
-        """
-    
-        bwt = str()
-        
-        for i in range(len(self)):
-            bwt += text[self[i] - 1]
-    
-        return bwt
-
-def bwt_search(bwt, text, pattern):
-    """
-    Find occurrences of a pattern in a text.
-    Input:
-        bwt: a BW-transformer (string)
-        text: a string
-        pattern: a string
-    Output:
-        list of positions where the pattern occurs
-    """
-    
-    return None
-    
-    
-    
-    
+            children = trie.children
+            
+            if children:
+                for child in children:
+                    self.from_trie(child)
+            else:
+                self.extend(trie.index)
