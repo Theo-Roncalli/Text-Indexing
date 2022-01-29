a7538e66335e84a7398f2491ee16d4a33a09fd2f
Théo Roncalli <theo.roncalli@yahoo.fr>
2022-01-29 14:12:13 +0100

    :white_check_mark: Check if pattern search works well

--- Python/example-sa.py
+++ Python/example-sa.py
@@ -4,11 +4,19 @@ import SuffixArray
 
 print("\n------------------------Suffix Array------------------------\n")
 
-text = "ananasbanana $"
-pattern = "ana"
+text = None
+path = "romeo_juliette.txt"
+pattern = "misadventured"
+
+if text and path:
+    raise TypeError("SuffixArray __init__() takes either text or path argument")
+elif not text and path:
+            with open(path, 'r') as file:
+                text = file.read()
+            if text[-1] != "$": text += "$"
 
 suffix_array = SuffixArray.SuffixArray(text)
 print("Suffix Array = ", suffix_array, sep = "")
-
 occurrences = suffix_array.pattern_search(text, pattern)
-print("We can find the pattern \"", pattern, "\" at positions ", occurrences, sep = "")
+print("We can find the pattern \"", pattern, "\" at position(s) ", occurrences, sep = "")
+print("Indeed, at position ", occurrences[0], " we have: ", text[occurrences[0]:occurrences[0]+len(pattern)], sep = "")
