a12c3f6d9747e52a24cb2960072b63981ef69aa0
Théo Roncalli <theo.roncalli@yahoo.fr>
2022-01-30 13:07:39 +0100

    :sparkles: Add compression, change the philosphy of using the BurrowsWheeler class

--- Python/BurrowsWheeler.py
+++ Python/BurrowsWheeler.py
@@ -2,20 +2,48 @@
 
 import SuffixArray as sa
 
-class BurrowsWheeler(str):
+class BurrowsWheeler():
     
-    def __new__(self, text, suffix_array = None):
+    def __init__(self, text, suffix_array = None, compression = None):
         
         if text[-1] != "$": text += "$"
 
         if not suffix_array:
             suffix_array = sa.SuffixArray(text = text)
-        bwt = str()
+        self.bwt = str()
         for idx in suffix_array:
-            bwt += text[idx-1]
+            self.bwt += text[idx-1]
         
-        return str.__new__(self, bwt)
-#        return sa.SaStrOrder.__new__(self, bwt)
-        
+        if not compression:
+            self.compression = False
+        else:
+            self.compression = True
+            self.compress()
     
-
+    def __str__(self):
+        return self.bwt
+    
+    def compress(self, encode="run-length"):
+        
+        if self.compression is True:
+            raise ValueError("The bw-transform is already compressed.")
+        elif encode == "run-length":
+            bwt = ""
+            last_letter = None
+            count = 1
+        for letter in self.bwt:
+            if letter == last_letter:
+                count += 1
+            elif letter != last_letter and count > 1:
+                bwt += (str(count) + letter)
+                last_letter = letter
+                count = 1
+            elif letter != last_letter and count == 1:
+                bwt += letter
+                last_letter = letter
+        if count > 1:
+            bwt += str(count)
+        self.bwt = bwt
+        
+        
+            
