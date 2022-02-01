424da20dd439f56e77b7a8f8287edfd26b472546
Théo Roncalli <theo.roncalli@yahoo.fr>
2022-02-01 11:48:41 +0100

    :art: Modification of the code structure

--- Python/BurrowsWheeler.py
+++ Python/BurrowsWheeler.py
@@ -4,12 +4,27 @@ ALPHABET = [chr(i) for i in range(128)]
 ALPHABET.insert(0, ALPHABET.pop(ALPHABET.index("$")))
 for i in range(0,10): ALPHABET.remove(str(i))
 POS = {c : p for (p, c) in enumerate(ALPHABET)}
+NEXT_LETTER = {ALPHABET[i]: ALPHABET[i+1] for i in range(len(ALPHABET) - 1)}
 
 import SuffixArray as sa
 
 class BurrowsWheeler():
     
     def __init__(self, text, suffix_array = None, compression = None):
+        
+        def counts(self):
+            """
+            Returns an array of length of the alphabet such that the i-th element corresponds
+            to the number of symbols smaller than the i-th symbol in the lexigraphical order.
+            """
+            
+            self.counts = dict()
+            self.counts[ALPHABET[0]] = 0
+            last_letter = ALPHABET[0]
+            
+            for letter in ALPHABET[1:]:
+                self.counts[letter] = self.counts[last_letter] + self.bwt.count(last_letter)
+                last_letter = letter
         
         if text[-1] != "$": text += "$"
 
@@ -19,12 +34,14 @@ class BurrowsWheeler():
         for idx in suffix_array:
             self.bwt += text[idx-1]
         
+        counts(self)
+        
         if not compression:
             self.compression = None
         else:
             self.compression = "run-length"
             self.compress()
-    
+        
     def __str__(self):
         return self.bwt
     
@@ -75,60 +92,58 @@ class BurrowsWheeler():
             if count != 0: bwt = bwt + last_letter * (count-1)
             self.bwt = bwt
     
-    def toString(self):
+    def toString(self, option = "classic"):
         """
         Implement decompression of a run-length encoding bwt text.
         """
         
-        def compute_first_column(occs):
-            F = {}
-            totc = 1
-            F["$"] = (0, 1)
-            del occs["$"]
-            for letter in sorted(occs.keys()):
-                count = occs[letter]
-                F[letter] = (totc, totc + count)
-                totc += count
-            return F
+        if option == "classic":
+            
+            def compute_first_column(occs):
+                F = {}
+                totc = 1
+                F["$"] = (0, 1)
+                del occs["$"]
+                for letter in sorted(occs.keys()):
+                    count = occs[letter]
+                    F[letter] = (totc, totc + count)
+                    totc += count
+                return F
 
-        def rank(bwt):
-            occs = dict()
-            ranks = []
-            for letter in bwt:
-                if letter not in occs:
-                    occs[letter] = 0
-                ranks.append(occs[letter])
-                occs[letter] += 1
-            return ranks, occs
+            def rank(bwt):
+                occs = dict()
+                ranks = []
+                for letter in bwt:
+                    if letter not in occs:
+                        occs[letter] = 0
+                    ranks.append(occs[letter])
+                    occs[letter] += 1
+                return ranks, occs
+            
+            bwt = list(self.bwt)
+            ranks, occs = rank(bwt)
+            F = compute_first_column(occs)
+            text = "$"
+            row = 0
+            
+            while bwt[row] != "$":
+                letter = bwt[row]
+                text = letter + text
+                row = F[letter][0] + ranks[row]
+                
+            return text
         
-        bwt = list(self.bwt)
-        ranks, occs = rank(bwt)
-        F = compute_first_column(occs)
-        text = "$"
-        row = 0
-    
-        while bwt[row] != "$":
-            letter = bwt[row]
-            text = letter + text
-            row = F[letter][0] + ranks[row]
-        
-        return text
-
-    def count(self):
-        """
-        Returns an array of length of the alphabet such that the i-th element corresponds
-        to the number of symbols smaller than the i-th symbol in the lexigraphical order.
-        """
-        
-        counts = dict()
-        counts[ALPHABET[0]] = 0
-        last_letter = ALPHABET[0]
-        
-        for letter in ALPHABET[1:]:
-            counts[letter] = counts[last_letter] + self.bwt.count(last_letter)
-            last_letter = letter
-        
-        return counts
+        elif option == "Ferragina":
+            
+            LastToFirst = self.ltf_mapping(1)
+            text = self.bwt[0] + "$"
+            
+            for _ in range(len(self.bwt) - 3):
+                letter = self.bwt[LastToFirst]
+                text = letter + text
+                LastToFirst = self.ltf_mapping(LastToFirst)
+                
+            return text
     
     def occ(self, letter, pos):
         """
@@ -137,17 +152,42 @@ class BurrowsWheeler():
         
         return self.bwt[:pos].count(letter)
     
-    def ltf_mapping(self, index, counts = None):
+    def ltf_mapping(self, index):
         """
         Performs last-to-first column mapping.
         """
         
-        if counts is None:
-            counts = self.count()
-        
         letter = self.bwt[index]
         
-        return counts[letter] + self.occ(letter, index)
+        return self.counts[letter] + self.occ(letter, index)
+    
+    def occurrence_number(self, pattern):
+        
+        letter = pattern[len(pattern) - 1]
+        First = self.counts[letter] + 1; Last = self.counts[NEXT_LETTER[letter]];
+        
+        for i in reversed(range(len(pattern) - 1)):
+            letter = pattern[i]
+            First = self.counts[letter] + self.occ(letter, First-1) + 1
+            Last = self.counts[letter] + self.occ(letter, Last)
+            if Last < First: break
+        
+        return None if Last < First else First, Last
+
+
+
+def pattern_search(text, pattern):
+    
+    size_text = len(text)
+    size_pattern = len(pattern)
+    pos = list()
+    
+    for index in range(size_text - size_pattern):
+        if text[index:index+size_pattern] == pattern:
+            pos.append(index)
+
+    return pos
+        
         
         
         
